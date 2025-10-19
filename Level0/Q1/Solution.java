6ced7b8c1b37a802fd24354a59f579e0b31b93ff
kangsehun96 <kangsehun96@gmail.com>
2025-10-19 22:49:21 +0900

    Update Level0/Q1.md

--- Level0/Q1/Solution.java
+++ Level0/Q1/Solution.java
@@ -4,6 +4,7 @@ public class Solution {
     public static void main(String[] args) {
         String message = "Let's go";
 
+        // 이스케이프 시퀀스 \n을 이용하여 개행
         System.out.println("3\n2\n1");
         System.out.println(message);
     }
